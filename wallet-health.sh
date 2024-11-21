55e42b15be3365a696d5fbfb4778d17bacf7a8db
Alex Gartner <git@agartner.com>
2024-11-21 20:15:30 +0000

    fix wallet name

--- wallet-health.sh
+++ wallet-health.sh
@@ -2,10 +2,15 @@
 
 set -eo pipefail
 
+wallet_name=default
+if [[ -n $1 ]]; then
+  wallet_name=$1
+fi
+
 # this script checks if a wallet exists and is not scanning
 
 echo "Getting wallet info"
-wallet_info=$(bitcoin-cli -rpcwallet=$1 getwalletinfo)
+wallet_info=$(bitcoin-cli -rpcwallet=${wallet_name} getwalletinfo)
 
 if [[ $(echo "$wallet_info" | jq -r '.scanning') == "true" ]]; then
   echo "Error: Wallet is currently scanning" >&2
