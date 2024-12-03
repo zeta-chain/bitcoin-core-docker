c0f3ec3f7633040782cb929db529a0cad0ff428e
morde08 <peterlee0808@gmail.com>
2024-12-03 15:00:56 -0800

    Add gitignore file (#3)
    * Add rpc whitelist
    
    * fix password hash format
    
    * add port to script
    
    * add gitignore
    
    * simplify
    
    ---------
    
    Co-authored-by: Alex Gartner <git@agartner.com>

--- examples/testnet4.sh
+++ examples/testnet4.sh
@@ -9,4 +9,5 @@ docker run -d \
   -e ADMIN_RPC_PASSWORD=admin \
   -e WALLET_NAME=default \
   -e WALLET_ADDRESS=tb1qfm8a8pxer0kmfa4xlk34e44xpr8g46ae0v04dw \
+  -p 48332:48332 \
   bitcoin-core-docker /opt/wallet.sh
