7ce35819e4715f56ac89949f5f56fd1ecbcc7181
Alex Gartner <git@agartner.com>
2024-12-03 13:53:48 -0800

    Add rpc whitelist (#2)
    * Add rpc whitelist
    
    * fix password hash format

--- examples/testnet4.sh
+++ examples/testnet4.sh
@@ -5,6 +5,8 @@ docker run -d \
   -e CHAIN=testnet4 \
   -e RPC_USER=default \
   -e RPC_PASSWORD=default \
+  -e ADMIN_RPC_USER=admin \
+  -e ADMIN_RPC_PASSWORD=admin \
   -e WALLET_NAME=default \
   -e WALLET_ADDRESS=tb1qfm8a8pxer0kmfa4xlk34e44xpr8g46ae0v04dw \
   bitcoin-core-docker /opt/wallet.sh
