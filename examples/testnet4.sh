40daeb42f42a802db6d98aff175e4783f961927d
Alex Gartner <git@agartner.com>
2025-03-20 08:42:15 -0700

    Add EXTRA_ADDRESSES variable (#21)

--- examples/testnet4.sh
+++ examples/testnet4.sh
@@ -9,5 +9,6 @@ docker run -d \
   -e ADMIN_RPC_PASSWORD=admin \
   -e WALLET_NAME=default \
   -e WALLET_ADDRESS=tb1qfm8a8pxer0kmfa4xlk34e44xpr8g46ae0v04dw \
+  -e EXTRA_ADDRESSES=tb1qz7n05rg9swm97h4lyyx2uuphzm0cxd6sj529k4 \
   -p 48332:48332 \
   bitcoin-core-docker /opt/wallet.sh
